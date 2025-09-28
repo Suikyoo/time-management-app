119747382b011dae62234dee12dbacab5f9efbde
Suikyoo <judeanthony02sayson@gmail.com>
2025-09-28 22:02:51 +0800

    fixed calendar size

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,75 @@
 import {Day, getDateAfter, getFinalDate} from "@/lib/calendar/calendar";
-import {useState} from "react";
-import { View, Text, StyleSheet, Button, FlatList } from "react-native";
+import {useRef, useState} from "react";
+import { View, Text, StyleSheet, Button, FlatList, ViewabilityConfig, useWindowDimensions } from "react-native";
 
 import Calendar from "@/components/Calendar";
 
 export default function Index() {
   const [date, setDate] = useState(new Date());
+  const {width} = useWindowDimensions();
 
+  const getPreviousMonth = (date: Date): Date => {
+    return getDateAfter(date, -1 * Day * 30 + (getFinalDate(date) % 30) % 2);
+  }
+
+  const getNextMonth = (date: Date): Date => {
+    return getDateAfter(date, Day * getFinalDate(date));
+  }
+
+  const [viewData, setViewData] = useState([getPreviousMonth(date), date, getNextMonth(date)])
+  const flatListRef = useRef<FlatList<Date>>(null);
+
+  const viewabilityConfig: ViewabilityConfig = {minimumViewTime: 300, itemVisiblePercentThreshold: 50, waitForInteraction: true};
+  const styles = StyleSheet.create({
+    container: {
+      boxSizing: "border-box",
+      padding: 20,
+
+    },
+    calendarList: {
+      width: "100%",
+
+    }
+  })
 
   return (
-      <View>
+      <View style={styles.container}>
 
         <FlatList 
-        data={[
-        getDateAfter(date, Day * 30 + (getFinalDate(date) % 30) % 2), 
-        date, 
-        getDateAfter(date, Day * getFinalDate(date))
-        ]} 
+        ref={flatListRef}
+        data={viewData} 
         renderItem={item => (
             <Calendar date={item.item}/> 
             )}
         horizontal
         pagingEnabled
         style={styles.calendarList}
+        keyExtractor={(item) => item.getFullYear().toString() + "-" + item.getMonth().toString()}
+        contentContainerStyle={{width: "300%"}}
+        onViewableItemsChanged={(i) => {
+          const active_item = i.changed.find((v) => v.isViewable);
+
+          if (!active_item || active_item.index == 1) {
+            return; 
+          }
+
+          if (active_item.index == 0) {
+            //-----------------------------------------------used to be viewData[0]
+            setViewData([getPreviousMonth(active_item.item), active_item.item, viewData[1]])
+          }
+          else if (active_item.index == 2) {
+            setViewData([viewData[1], active_item.item, getNextMonth(active_item.item)])
+          }
+
+          requestAnimationFrame(() => {
+            flatListRef.current?.scrollToIndex({index: 1, animated: false});
+
+          })
+
+        }}
+        viewabilityConfig={viewabilityConfig}
         />
       </View>
 
       );
 }
-
-const styles = StyleSheet.create({
-calendarList: {
-
-}
-    })
--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,5 +1,5 @@
 import {getPage, month_names, day_names} from "@/lib/calendar/calendar";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, useWindowDimensions } from "react-native";
 
 interface CalendarProp {
   date: Date;
@@ -7,84 +7,85 @@ interface CalendarProp {
 export default function Calendar({date}: CalendarProp) {
   const page = getPage(date);
 
+  const styles = StyleSheet.create({
+    container: {
+      flexGrow: 1,
+      width: `${100/3}%`,
+      justifyContent: "flex-start",
+      alignItems: "center",
+
+    },
+    calendar: {
+      width: "100%",
+      display: "flex",
+      flexWrap: "wrap",
+      flexDirection: "row",
+      alignItems: "center",
+
+    },
+    item: {
+      textAlign: "center",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: `${100/7}%`,
+      height: 50,
+      outlineWidth: 1,
+      outlineOffset: -2,
+      outlineColor: "#cccccc",
+      borderRadius: 5, 
+      boxSizing: "border-box",
+      padding: 2,
+
+    },
+
+    header: {
+      display: "flex",
+      fontWeight: 800,
+      fontSize: 200,
+      justifyContent: "center",
+      alignItems: "center",
+      width: `${100/7}%`,
+      height: 30,
+      boxSizing: "border-box",
+      padding: 2,
+    },
+
+    nav: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+    }
+
+  });
+ 
   return (
-      <View style={styles.container}>
-        <View style={styles.nav}>
-          <Text>{month_names[page.month]}</Text>
-        </View>
-        <View style={styles.calendar}>
-        {
-        day_names.map((v, index) => (
-              <View key={index} style={styles.header}>
-              <Text>{v.substring(0, 3)}</Text>
-              </View>
-              )
-            )
-        }
-        {
-        page.days.map((d, index) => (
-              <View key={index} style={[styles.item, index < page.offset ? {outlineWidth: 0} : {outlineWidth: 1}]}>
-              { index < page.offset ? <Text style={{color: "#888888"}}>{d}</Text> : <Text>{d}</Text> }
-              </View>
-              )
-            )
-
-        }
-        </View>
+    <View style={styles.container}>
+      <View style={styles.nav}>
+      <Text>{month_names[page.month]}</Text>
       </View>
-      );
+      <View style={styles.calendar}>
+      {
+        day_names.map((v, index) => (
+          <View key={index} style={styles.header}>
+          <Text>{v.substring(0, 3)}</Text>
+          </View>
+        )
+                     )
+      }
+      {
+        page.days.map((d, index) => (
+          <View key={index} style={[styles.item, index < page.offset ? {outlineWidth: 0} : {outlineWidth: 1}]}>
+          { index < page.offset ? <Text style={{color: "#888888"}}>{d}</Text> : <Text>{d}</Text> }
+          </View>
+        )
+                     )
+
+      }
+      </View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-container: {
-boxSizing: "border-box",
-padding: 5,
-display: "flex",
-justifyContent: "center",
-alignItems: "center",
 
-},
-calendar: {
-width: "100%",
-display: "flex",
-flexWrap: "wrap",
-flexDirection: "row",
-alignItems: "center",
-
-},
-item: {
-textAlign: "center",
-display: "flex",
-justifyContent: "center",
-alignItems: "center",
-width: `${100/7}%`,
-height: 50,
-outlineWidth: 1,
-outlineOffset: -2,
-outlineColor: "#cccccc",
-borderRadius: 5, 
-boxSizing: "border-box",
-padding: 2,
-
-      },
-
-header: {
-display: "flex",
-fontWeight: 800,
-fontSize: 200,
-justifyContent: "center",
-alignItems: "center",
-width: `${100/7}%`,
-height: 30,
-boxSizing: "border-box",
-padding: 2,
-},
-
-nav: {
-display: "flex",
-flexDirection: "row",
-justifyContent: "center",
-alignItems: "center",
-}
-
-});
